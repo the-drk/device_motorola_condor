a30ab907bffa7c1d2857c1fd29ad62acec0cb56e
Ashutosh Sathe <2ashutoshbs@gmail.com>
2018-01-13 11:52:10 +0530

    I didn't merge properly(?)

--- lineage.mk
+++
@@ -1,9 +0,0 @@
-$(call inherit-product, device/motorola/condor/full_condor.mk)
-
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_condor
-PRODUCT_RELEASE_NAME := MOTO E
-PRODUCT_MODEL := Moto E
